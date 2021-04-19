7882de9c580f27e2d43cb677f18fc8464465adab
tnebes <47086978+tnebes@users.noreply.github.com>
2021-04-19 20:05:48 +0200

    Invoices can now be searched by token

--- invoiceGenerator/src/main/java/invoiceGenerator/controller/InvoiceHandler.java
+++ invoiceGenerator/src/main/java/invoiceGenerator/controller/InvoiceHandler.java
@@ -1,12 +1,15 @@
 package invoiceGenerator.controller;
 
+import invoiceGenerator.model.Customer;
 import invoiceGenerator.model.Invoice;
 import invoiceGenerator.model.Status;
 import invoiceGenerator.model.TransactionType;
 import invoiceGenerator.util.InvoiceGeneratorException;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class InvoiceHandler extends Handler<Invoice> {
 
@@ -18,8 +21,50 @@ public class InvoiceHandler extends Handler<Invoice> {
 
     @Override
     public List<Invoice> getData(String token) throws InvoiceGeneratorException {
-        //TODO add token
-        return getData();
+        token = token.toLowerCase();
+        List<Invoice> invoices = getData();
+        List<Invoice> returnInvoices = new ArrayList<>();
+        returnInvoices.addAll(invoices);
+        // absolutely cursed.
+        for (Invoice invoice : invoices) {
+            if (invoice.getId().toString().contains(token)) {
+                continue;
+            }
+            if (invoice.getCustomer() != null) {
+                if (invoice.getCustomer().isType() == Customer.NATURAL_PERSON) {
+                    if (invoice.getCustomer().getFirstName().toLowerCase().contains(token)) {
+                        continue;
+                    }
+                    if (invoice.getCustomer().getLastName().toLowerCase().contains(token)) {
+                        continue;
+                    }
+                    if (invoice.getCustomer().getMiddleName().toLowerCase().contains(token)) {
+                        continue;
+                    }
+                    if (invoice.getCustomer().getNationalIdNumber().contains(token)) {
+                        continue;
+                    }
+                } else {
+                    if (invoice.getCustomer().getName().toLowerCase().contains(token)) {
+                        continue;
+                    }
+                    if (invoice.getCustomer().getVATID().toLowerCase().contains(token)) {
+                        continue;
+                    }
+                }
+                if (invoice.getId().toString().contains(token)) {
+                    continue;
+                }
+                if (invoice.getAmountDue().toString().contains(token)) {
+                    continue;
+                }
+                if (invoice.getAmountPaid().toString().contains(token)) {
+                    continue;
+                }
+            }
+            returnInvoices.remove(invoice);
+        }
+        return returnInvoices;
     }
 
     public List<Invoice> getData(Status status) throws InvoiceGeneratorException {
@@ -42,23 +87,20 @@ public class InvoiceHandler extends Handler<Invoice> {
     }
 
     public List<Invoice> getData(Status status, TransactionType transactionType, String token) throws InvoiceGeneratorException {
-        // TODO add token
-        List<Invoice> invoices = getData();
+        List<Invoice> invoices = getData(token);
         invoices.removeIf(invoice -> !invoice.getStatus().equals(status));
         invoices.removeIf(invoice -> !invoice.getTransactionType().equals(transactionType));
         return invoices;
     }
 
     public List<Invoice> getData(TransactionType transactionType, String token) throws InvoiceGeneratorException {
-        // TODO add token
-        List<Invoice> invoices = getData();
+        List<Invoice> invoices = getData(token);
         invoices.removeIf(invoice -> !invoice.getTransactionType().equals(transactionType));
         return invoices;
     }
 
     public List<Invoice> getData(Status status, String token) throws InvoiceGeneratorException {
-        // TODO add token
-        List<Invoice> invoices = getData();
+        List<Invoice> invoices = getData(token);
         invoices.removeIf(invoice -> !invoice.getStatus().equals(status));
         return invoices;
     }
@@ -74,7 +116,7 @@ public class InvoiceHandler extends Handler<Invoice> {
     }
 
     private void checkAmounts() throws InvoiceGeneratorException {
-        // TODO
+
     }
 
     @Override
